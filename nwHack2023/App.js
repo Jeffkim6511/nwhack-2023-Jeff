44da7d542dacff9c91dd151449e3544309e97542
Jeff Kim <jeffkim7@hotmail.com>
2023-01-21 21:07:24 -0800

    fixed equations but require firebase informatinon

--- nwHack2023/App.js
+++ nwHack2023/App.js
@@ -6,17 +6,14 @@ import { StyleSheet, Text, View } from 'react-native';
 addwater(int intakeW) {
   let water = ._____; 
   let newWater = 1.25 * (water + intake);
-  if (newWater < weight){
   return newWater
-  } else {
-    weight
   }
-}
+
 
 addweight(int intakeF) {
   let food = ._____; 
   let newfood = (food + intakeF);
-  return 66.5 + (9.663 * .Weight) + (1.850 * Height) - (4.676 * age)
+  return 66.5 + (9.663 * .Weight) + (1.850 * .Height) - (4.676 * .age)
 }
 
 export default function App() {
